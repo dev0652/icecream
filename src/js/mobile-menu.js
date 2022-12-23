16089e0b38ce03a1ee69e1d0aa4e9f2dab7c6ed0
UA-DmitriyS <tavluy86@gmail.com>
2022-12-24 00:04:23 +0200

    проверка

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -18,7 +18,6 @@
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
