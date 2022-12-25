a32011f0d3119e8f9c65b988cdfb1d8917fa4bab
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2022-12-25 20:42:53 +0200

    add hero animation

--- src/js/scroll-trigger.js
+++ src/js/scroll-trigger.js
@@ -1,22 +1,3 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const benefits__icon = entry.target.querySelectorAll('.benefits__icon');
-    // const benefits__icon = entry.target.querySelector('.benefits__icon');
-
-    if (entry.isIntersecting) {
-      benefits__icon.forEach(x => x.classList.add('text-focus-in'));
-      // benefits__icon.classList.add('text-focus-in');
-      return; // if we added the class, exit the function
-    }
-
-    // We're not intersecting, so remove the class!
-    // benefits__icon.forEach(x => x.classList.remove('text-focus-in'));
-    // benefits__icon.classList.remove('text-focus-in');
-  });
-});
-
-observer.observe(document.querySelector('.benefits__list'));
-
 // Products
 const observerproduct = new IntersectionObserver(entries => {
   entries.forEach(entry => {
@@ -37,22 +18,22 @@ const observerproduct = new IntersectionObserver(entries => {
 
 observerproduct.observe(document.querySelector('.about__description'));
 
-// about
-// const observerabout = new IntersectionObserver(entries => {
+// /benefits
+// const observer = new IntersectionObserver(entries => {
 //   entries.forEach(entry => {
-//     const product__card = entry.target.querySelectorAll('.product-card');
+//     const benefits__icon = entry.target.querySelectorAll('.benefits__icon');
 //     // const benefits__icon = entry.target.querySelector('.benefits__icon');
 
 //     if (entry.isIntersecting) {
-//       product__card.forEach(x => x.classList.add('product-image-pulsing'));
+//       benefits__icon.forEach(x => x.classList.add('text-focus-in'));
 //       // benefits__icon.classList.add('text-focus-in');
 //       return; // if we added the class, exit the function
 //     }
 
 //     // We're not intersecting, so remove the class!
-//     product__card.forEach(x => x.classList.remove('product-image-pulsing'));
+//     // benefits__icon.forEach(x => x.classList.remove('text-focus-in'));
 //     // benefits__icon.classList.remove('text-focus-in');
 //   });
 // });
 
-// observerabout.observe(document.querySelector('.product-cards'));
+// observer.observe(document.querySelector('.benefits__list'));
