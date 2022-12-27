69ef34ba36d3097a3a0198cf3959bf51c577a537
audi0652 <113797010+dev0652@users.noreply.github.com>
2022-12-27 14:57:28 +0200

    temp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './js/mobile-menu';
-import './js/slider';
-import './js/modal';
-import './js/dynamic-padding';
-import './js/disabled-buttons';
-import './js/scroll-trigger';
 
-// import './js/xxxxxxx';
+import './js/dynamic-padding';
+import './js/scroll-trigger';
+import './js/slider';
+import './js/mobile-menu';
+import './js/disabled-buttons';
+import './js/modal';
+
