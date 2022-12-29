39c411075a854e6d02fbcfba9476cdb36e960047
dev0652 <113797010+dev0652@users.noreply.github.com>
2022-12-29 10:21:56 +0200

    temp

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 import './js/dynamic-padding';
 import './js/scroll-trigger';
 import './js/mobile-menu';
-import './js/slider';
 import './js/modal';
 import './js/trap-focus-in-modal';
 import './js/disabled-buttons';
+import './js/slider';
 
 
