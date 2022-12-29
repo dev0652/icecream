48a3717968264c603d01dafd56c6194ab63dac23
audi0652 <113797010+dev0652@users.noreply.github.com>
2022-12-29 19:11:04 +0200

    temp

--- src/js/disabled-buttons.js
+++ src/js/disabled-buttons.js
@@ -1,21 +1,18 @@
 const formInput = document.querySelector('[required]');
 const formButton = document.querySelector('button[type="submit"]');
 
-// the default state is 'disabled'
 formButton.disabled = true;
 
-// alternative is to use "change" - explained below
 formInput.addEventListener("keyup", buttonState);
 
 function buttonState() {
 	if (document.querySelector('[required]').value === "") {
-		formButton.disabled = true; // return disabled as true whenever the input field is empty
+		formButton.disabled = true;
 	} else {
-		formButton.disabled = false; // enable the button once the input field has content
+		formButton.disabled = false;
 	}
 }
 
-// just verifying that the button has been clicked
 formButton.addEventListener("click", () => {
 	console.log("You entered:", document.querySelector(".form-input").value);
 });
