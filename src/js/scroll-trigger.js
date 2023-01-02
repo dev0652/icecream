70c30e58cad7ac8cbaecf0d0f8cc073d4b43de6f
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-01-02 13:10:30 +0200

    Finished counters

--- src/js/scroll-trigger.js
+++ src/js/scroll-trigger.js
@@ -1,19 +1,20 @@
-// Products
+// About
+
 const observerproduct = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const image__wrapper = entry.target.querySelector('.about__image-wrapper');
-    const about__image = entry.target.querySelector('.about__image');
+	entries.forEach(entry => {
+		const image__wrapper = entry.target.querySelector('.about__image-wrapper');
+		const about__image = entry.target.querySelector('.about__image');
 
-    if (entry.isIntersecting) {
-      image__wrapper.classList.add('bounce-in-top');
-      about__image.classList.add('bounce-in-top');
-      return; // if we added the class, exit the function
-    }
+		if (entry.isIntersecting) {
+			image__wrapper.classList.add('bounce-in-top');
+			about__image.classList.add('bounce-in-top');
+			return; // if we added the class, exit the function
+		}
 
-    // We're not intersecting, so remove the class!
-    // image__wrapper.classList.remove('bounce-in-top');
-    // about__image.classList.remove('bounce-in-top');
-  });
+		// We're not intersecting, so remove the class!
+		// image__wrapper.classList.remove('bounce-in-top');
+		// about__image.classList.remove('bounce-in-top');
+	});
 });
 
 observerproduct.observe(document.querySelector('.about__description'));
