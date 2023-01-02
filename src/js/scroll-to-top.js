48d066b1e60132be362f60d3565c6ada6ce9b960
audi0652 <113797010+dev0652@users.noreply.github.com>
2023-01-02 18:26:42 +0200

    Fixed window.onscroll events

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -1,14 +1,7 @@
 // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
-
-// When the user scrolls down 20px from the top of the document, show the button
-
-// !!! moved this to window.js
 // window.onscroll = function () { scrollFunction() };
-
-
 window.addEventListener('scroll', scrollFunction);
-// window.addEventListener('scroll', startCounterAnimation);
 
 
 function scrollFunction() {
@@ -22,7 +15,6 @@ function scrollFunction() {
 	}
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
